410d07c562b16094151e1d4eb9a21b688b9d15d0
xKodyDrake <drakek1@wncc.edu>
2024-10-24 23:09:33 -0600

    Adjust hitboxes, new meteors and animations
    I adjusted the hitboxes for the player and the mobs as well as added rotation to the mobs and adjusting the spawn location for the new randomly generated meteors

--- shmup.py
+++ shmup.py
@@ -34,6 +34,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (50, 38))   # Scales player model
         self.image.set_colorkey(BLACK)                              # Removes black box around graphic
         self.rect = self.image.get_rect()
+        self.radius = 19
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)  # Draws red circle to test radius / hitbox
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -62,23 +64,44 @@ class Player(pygame.sprite.Sprite):
         all_sprites.add(bullet)
         bullets.add(bullet)
 
-# Class for mob
+# Class for mob (image_orig is used to rotate without losing pixels / data)
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = meteor_img
-        self.image.set_colorkey(BLACK)
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)  # Draws red circle to test radius / hitbox
         
         # Random mob spawn
         self.rect.x = random.randrange(WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-150, -100)
 
         # Random speed
         self.speedy = random.randrange(3, 7)
         self.speedx = random.randrange(-3, 3)
 
+        # Set rotation of mob, use clock / game ticks to decide when to turn again
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360    # Use modulus to loop back to 1 after reaching 360 degrees of rotation (360 % 360 = 0, 361 % 360 = 1, etc.)
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+
+
     def update(self):
+        self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         
@@ -111,8 +134,14 @@ class Bullet(pygame.sprite.Sprite):
 background = pygame.image.load(path.join(img_dir, "starfield.png")).convert()
 background_rect = background.get_rect()
 player_img = pygame.image.load(path.join(img_dir, "playerShip1_green.png")).convert()
-meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
+# meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()  # Original meteor image (used only one image)
 bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+meteor_images = []  # List to store meteor images to randomly pick from
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_big2.png', 'meteorBrown_med1.png',
+               'meteorBrown_med3.png', 'meteorBrown_small1.png', 'meteorBrown_small2.png',
+               'meteorBrown_tiny1.png',]
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
 
 
 
@@ -127,6 +156,9 @@ for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
+
+# Initialize score and set to 0
+score = 0
 
 # Game loop
 running = True
@@ -158,8 +190,9 @@ while running:
         all_sprites.add(m)
         mobs.add(m)
 
-    # Check if mob hit player (player, group hit, False doesnt delete hit enemy)
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    # Check if mob hit player (player, group hit, False doesnt delete hit enemy, collide_circle changes hitbox type from the rectangle base setting to circles)
+    # From Axis-aligned Bounding Box (AABB - Rectangle - Default) ---> Circular Bounding Box (CBB - Circle - declared in spritecollide)
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
 
     # If hits anything, close program
     if hits:
